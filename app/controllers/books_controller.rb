2bc1cca782c7218438528df498b9dd793417564d
EC2 Default User <ec2-user@ip-172-31-20-241.ec2.internal>
2022-05-14 02:55:15 +0000

    [Update]各画面の編集、Bookモデル編集画面の新規作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to book_path(@book)
   end
 
   def index
@@ -18,8 +18,12 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
   def destroy
-    @book = book.find(params[:id])
+    @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
   end
