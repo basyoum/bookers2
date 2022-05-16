59ba6baecc5511af8d95970c332f4d711444373a
EC2 Default User <ec2-user@ip-172-31-20-241.ec2.internal>
2022-05-16 08:01:50 +0000

    [Add]Rspecテストエラーによる訂正、アバウト・トップ画面ルート変更、新規登録でemailが入力できないを訂正、投稿フォームに投稿済みデータが残るを訂正、投稿編集画面Backリンク訂正、他人の編集画面へ遷移しないようにする、アイコン名変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  root to:'homes#top'
+  get '/' => 'homes#top'
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about', as: 'about'
   devise_for :users
 
   resources :books
   resources :users, only: [:show, :index, :edit, :update]
 
-  get 'homes/top'
-  get 'homes/about' => 'homes#about', as: 'about'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
